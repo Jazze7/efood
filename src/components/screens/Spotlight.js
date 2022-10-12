689b6c8385ef3dcbb8ed7ef78b690d12b4e51d02
Jazzi7 <jassimsm9@gmail.com>
2022-10-12 19:41:06 +0530

    sidebar styling completed

--- src/components/screens/Spotlight.js
+++ src/components/screens/Spotlight.js
@@ -7,52 +7,135 @@ export default function Spotlight() {
     <>
       <MainContainer>
         <LeftContainer>
-          <LogoContainer>
-            <LogoImage
-              src={require("../../Assets/images/logo.svg").default}
-            ></LogoImage>
-          </LogoContainer>
+          <ContentContainer>
+            <LogoContainer>
+              <LogoImage
+                src={require("../../Assets/images/logo.svg").default}
+              ></LogoImage>
+            </LogoContainer>
 
-          <WidgetContainer>
-            <WidgetImage
-              src={require("../../Assets/images/menu.svg").default}
-            ></WidgetImage>
-            <WidgetImage
-              src={require("../../Assets/images/circlepiece.svg").default}
-            ></WidgetImage>
-            <WidgetImage
-              src={require("../../Assets/images/love.svg").default}
-            ></WidgetImage>
-            <WidgetImage
-              src={require("../../Assets/images/square.svg").default}
-            ></WidgetImage>
-            <WidgetImage
-              src={require("../../Assets/images/cart.svg").default}
-            ></WidgetImage>
-            <WidgetImage
-              src={require("../../Assets/images/settings.svg").default}
-            ></WidgetImage>
-            <WidgetImage
-              src={require("../../Assets/images/profile.svg").default}
-            ></WidgetImage>
-            <WidgetImage
-              src={require("../../Assets/images/exclamation.svg").default}
-            ></WidgetImage>
-          </WidgetContainer>
+            <WidgetContainer>
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/menu.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
 
-          <ProfilePic>
-            <Picture src={require("../../Assets/images/pic.jpg")}></Picture>
-          </ProfilePic>
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/circlepiece.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
+
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/love.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
+
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/square.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
+
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/cart.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
+
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/settings.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
+
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/profile.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
+
+              <WidgetImageContainer>
+                <WidgetImage
+                  src={require("../../Assets/images/exclamation.svg").default}
+                ></WidgetImage>
+              </WidgetImageContainer>
+            </WidgetContainer>
+
+            <ProfilePic>
+              <Picture src={require("../../Assets/images/pic.jpg")}></Picture>
+            </ProfilePic>
+          </ContentContainer>
         </LeftContainer>
+
+        {/* <MiddleContainer>
+
+        </MiddleContainer> */}
       </MainContainer>
     </>
   );
 }
-const MainContainer = styled.section``;
-const LeftContainer = styled.div``;
-const LogoContainer = styled.div``;
-const LogoImage = styled.img``;
-const WidgetContainer = styled.div``;
-const WidgetImage = styled.img``;
-const ProfilePic = styled.div``;
-const Picture = styled.img``;
+const MainContainer = styled.section`
+  height: 100vh;
+  padding: 20px 0;
+`;
+const LeftContainer = styled.div`
+  width: 10%;
+`;
+const ContentContainer = styled.div`
+  width: 40%;
+  margin: 0 auto;
+`;
+const LogoContainer = styled.div`
+  width: 100%;
+`;
+const LogoImage = styled.img`
+  width: 100%;
+  display: block;
+`;
+const WidgetContainer = styled.ul`
+  background: var(--orange);
+  margin: 60px 0;
+  padding: 20px 0;
+  border-radius: 50px;
+`;
+const WidgetImageContainer = styled.li`
+  width: 40%;
+  margin: 0 auto 20px;
+  position: relative;
+  &:last-child {
+    margin-bottom: 0;
+  }
+  &::after {
+    content: "";
+    position: absolute;
+    top: -7px;
+    left: -7px;
+    display: block;
+    width: 35px;
+    height: 35px;
+    cursor: pointer;
+    opacity: 0;
+    border-radius: 50%;
+    background-color: #FFFFFF;
+  }
+  &:hover {
+    &::after {
+       opacity:0.3;
+    }
+}
+`;
+const WidgetImage = styled.img`
+  width: 100%;
+  display: block;
+`;
+const ProfilePic = styled.div`
+  border-radius: 50%;
+  overflow: hidden;
+`;
+const Picture = styled.img`
+  width: 100%;
+  display: block;
+`;
