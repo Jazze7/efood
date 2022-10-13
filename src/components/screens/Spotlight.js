9ff1a3a3663750ad335b1d2f21ede5e6f0175186
Jazzi7 <jassimsm9@gmail.com>
2022-10-14 01:43:36 +0530

    Completed styling of entire screen

--- src/components/screens/Spotlight.js
+++ src/components/screens/Spotlight.js
@@ -47,6 +47,7 @@ export default function Spotlight() {
   return (
     <>
       <MainContainer>
+        {/* Structuring of left container starts */}
         <LeftContainer>
           <ContentContainer>
             <LogoContainer>
@@ -110,7 +111,9 @@ export default function Spotlight() {
             </ProfilePic>
           </ContentContainer>
         </LeftContainer>
+        {/* Structuring of left container ends */}
 
+        {/* Structuring of middle container starts */}
         <MiddleContainer>
           <Header>
             <HeadLeft>
@@ -151,7 +154,9 @@ export default function Spotlight() {
 
           <Content>{renderProduct()}</Content>
         </MiddleContainer>
+        {/* Structuring of Middle container ends */}
 
+        {/* Structuring of Right container starts */}
         <RightContainer>
           <DateContainer>
             <HeadLine>Your meal plan</HeadLine>
@@ -189,13 +194,14 @@ export default function Spotlight() {
                   <Name>Nasi Goreng Park Parjo </Name>
                   <Pricing>
                     <Discount>
-                      <Dollar>$</Dollar>8,10
+                      <DollarSign>$</DollarSign>8,10
                     </Discount>
                     <Actual>$10,10</Actual>
                   </Pricing>
                 </BreakFastRight>
               </BreakFastList>
-
+            </BreakFast>
+            <BreakFast>
               <BreakFastList>
                 <BreakFastLeft>
                   <BreakFastImgCont>
@@ -208,7 +214,7 @@ export default function Spotlight() {
                   <Name>Nasi Goreng Park Parjo </Name>
                   <Pricing>
                     <Discount>
-                      <Dollar>$</Dollar>8,10
+                      <DollarSign>$</DollarSign>8,10
                     </Discount>
                     <Actual>$10,10</Actual>
                   </Pricing>
@@ -216,8 +222,90 @@ export default function Spotlight() {
               </BreakFastList>
             </BreakFast>
           </BreakfastContainer>
+
+          <LunchContainer>
+            <CaptionContainer>
+              <Lemma>Lunch</Lemma>
+              <Being>Being ordered</Being>
+            </CaptionContainer>
+            <Time>01.30PM</Time>
+            <Lunch>
+              <LunchList>
+                <LunchLeft>
+                  <LunchImgCont>
+                    <LunchImg
+                      src={require("../../Assets/images/one.jpg")}
+                    ></LunchImg>
+                  </LunchImgCont>
+                </LunchLeft>
+                <LunchRight>
+                  <Name>Nasi Goreng Park Parjo </Name>
+                  <Pricing>
+                    <Discount>
+                      <DollarSign>$</DollarSign>8,10
+                    </Discount>
+                    <Actual>$10,10</Actual>
+                  </Pricing>
+                </LunchRight>
+              </LunchList>
+            </Lunch>
+
+            <Lunch>
+              <LunchList>
+                <LunchLeft>
+                  <LunchImgCont>
+                    <LunchImg
+                      src={require("../../Assets/images/one.jpg")}
+                    ></LunchImg>
+                  </LunchImgCont>
+                </LunchLeft>
+                <LunchRight>
+                  <Name>Nasi Goreng Park Parjo </Name>
+                  <Pricing>
+                    <Discount>
+                      <DollarSign>$</DollarSign>8,10
+                    </Discount>
+                    <Actual>$10,10</Actual>
+                  </Pricing>
+                </LunchRight>
+              </LunchList>
+            </Lunch>
+          </LunchContainer>
+
+          <DinnerContainer>
+            <CaptionContainer>
+              <Banner>Dinner</Banner>
+              <Order>Haven't ordered yet</Order>
+            </CaptionContainer>
+            <Time>09.30PM</Time>
+            <Dinner>
+              <DinnerList>
+                <DinnerLeft>
+                  <DinnerImgCont>
+                    <DinnerImg
+                      src={require("../../Assets/images/night.jpg")}
+                    ></DinnerImg>
+                  </DinnerImgCont>
+                </DinnerLeft>
+                <DinnerRight>
+                  <Name>Nasi Goreng Park Parjo </Name>
+                  <Pricing>
+                    <Discount>
+                      <DollarSign>$</DollarSign>8,10
+                    </Discount>
+                    <Actual>$10,10</Actual>
+                  </Pricing>
+                </DinnerRight>
+              </DinnerList>
+            </Dinner>
+          </DinnerContainer>
+          <DropContainer>
+            <Drop>Drop here to add</Drop>
+          </DropContainer>
         </RightContainer>
+        {/* Structuring of right container ends */}
       </MainContainer>
+      {/* Structuring ends*/}
     </>
   );
 }
@@ -243,7 +331,7 @@ const LogoImage = styled.img`
 `;
 const WidgetContainer = styled.ul`
   background: var(--orange);
-  margin: 60px 0;
+  margin: 120px 0;
   padding: 20px 0;
   border-radius: 50px;
 `;
@@ -487,7 +575,7 @@ const OrderButton = styled.button`
 const RightContainer = styled.aside`
   background: #eeeeee;
   width: 20%;
-  padding-left: 15px;
+  padding: 0 15px;
 `;
 const DateContainer = styled.div`
   padding-top: 20px;
@@ -501,43 +589,182 @@ const DateBar = styled.div`
   position: relative;
 `;
 const CalenderContainer = styled.div`
-  width: 23px;
+  width: 22px;
   top: 7px;
   position: absolute;
-  left: 3px;
+  left: 7px;
 `;
 const Calender = styled.img`
   display: block;
   width: 100%;
 `;
 const ArrowContainer = styled.div`
-  width: 20px;
+  width: 18px;
   position: absolute;
   top: 10px;
-  right: 26px;
+  right: 10px;
 `;
 const Arrow = styled.img`
   display: block;
   width: 100%;
 `;
-const BreakfastContainer = styled.div``;
-const CaptionContainer = styled.div``;
+const BreakfastContainer = styled.div`
+  // padding-bottom: 10px;
+`;
+const CaptionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+  margin-bottom: 20px;
+`;
 const Banner = styled.h5`
   font-size: 15px;
   font-weight: 700;
 `;
-const Purchased = styled.div``;
-const Time = styled.h4``;
+const Purchased = styled.div`
+  background: #d9ffe5;
+  color: #5cbe7b;
+  font-size: 11px;
+  border-radius: 10px;
+  padding: 3px 11px;
+  font-weight: 700;
+`;
+const Time = styled.h5`
+  font-size: 12px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
 const BreakFast = styled.ul``;
-const BreakFastList = styled.li``;
-const BreakFastLeft = styled.div``;
-const BreakFastImgCont = styled.div``;
+const BreakFastList = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  background-color: #fff;
+  border-radius: 6px;
+`;
+const BreakFastLeft = styled.div`
+  width: 35%;
+`;
+const BreakFastImgCont = styled.div`
+  width: 100%;
+  overflow: hidden;
+  border-bottom-left-radius: 6px;
+  border-top-left-radius: 6px;
+`;
 const BreakFastImg = styled.img`
   display: block;
   width: 100%;
 `;
-const BreakFastRight = styled.div``;
-const Name = styled.h5``;
-const Pricing = styled.div``;
-const Discount = styled.h4``;
-const Actual = styled.h6``;
+const BreakFastRight = styled.div`
+  width: 62%;
+`;
+const Name = styled.h5`
+  font-size: 10px;
+  font-weight: 700;
+  margin-bottom: 10px;
+`;
+const Pricing = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 60%;
+`;
+const DollarSign = styled.span`
+  display: inline-block;
+  font-size: 10px;
+  color: var(--orange);
+`;
+const Discount = styled.h4`
+  font-size: 12px;
+  font-weight: 700;
+`;
+const Actual = styled.h6`
+  font-size: 10px;
+  text-decoration: line-through;
+  color: #747474;
+`;
+const LunchContainer = styled.div``;
+const Lemma = styled.h5`
+  font-size: 15px;
+  font-weight: 700;
+`;
+const Being = styled.div`
+  font-size: 10px;
+  border-radius: 10px;
+  background: #fef3df;
+  color: #dfb77f;
+  padding: 3px 11px;
+  font-weight: 700;
+`;
+const Lunch = styled.ul``;
+const LunchList = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  background-color: #fff;
+  border-radius: 6px;
+`;
+const LunchLeft = styled.div`
+  width: 35%;
+`;
+const LunchImgCont = styled.div`
+  width: 100%;
+  overflow: hidden;
+  border-bottom-left-radius: 6px;
+  border-top-left-radius: 6px;
+`;
+const LunchImg = styled.img`
+  display: block;
+  width: 100%;
+`;
+const LunchRight = styled.div`
+  width: 62%;
+`;
+const DinnerContainer = styled.div``;
+const Order = styled.div`
+  font-size: 10px;
+  border-radius: 10px;
+  background: #ffeae9;
+  color: #d37670;
+  padding: 3px 11px;
+  font-weight: 600;
+`;
+const Dinner = styled.ul``;
+const DinnerList = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+  background-color: #fff;
+  border-radius: 6px;
+`;
+const DinnerLeft = styled.div`
+  width: 35%;
+`;
+const DinnerImgCont = styled.div`
+  width: 100%;
+  overflow: hidden;
+  border-bottom-left-radius: 6px;
+  border-top-left-radius: 6px;
+`;
+const DinnerImg = styled.img`
+  display: block;
+  width: 100%;
+`;
+const DinnerRight = styled.div`
+  width: 62%;
+`;
+const DropContainer = styled.div`
+  padding-top: 10px;
+`;
+const Drop = styled.div`
+  font-size: 12px;
+  border-radius: 10px;
+  border: 1px dashed #eac1a5;
+  padding: 10px 43px;
+  color: #d8772a;
+  font-weight: 600;
+  background-color: #ffecde;
+`;
